03194b0d125a6755cde303deceb50bf7f210fbb5
Th4umas <147378335+Th4umas@users.noreply.github.com>
2024-02-02 17:06:11 +0100

    update

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+const options = {
+    gutterPixels: 50,
+};
+
+const filterizr = new Filterizr('.portfolio-elements', options);
+
+let filtersList = document.querySelectorAll('.filters li')
+filtersList.forEach(function(filterItem) {
+    filterItem.addEventListener('click', function() {
+        document.querySelector('.filters .active').classList.remove('active')
+        this.classList.add('active')
+    })
+})
